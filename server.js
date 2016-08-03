a384001206324146437f22c2763b8285458e2d85
serfer89 <sergpavlov89@gmail.com>
2016-08-03 10:33:04 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,39 @@
+var sys = require('util');
+var net = require('net');
+var mqtt = require('mqtt');
+var mqtthost = '193.108.128.254';
+var options = {
+    host: mqtthost,
+    port: 1883,
+    protocolId: 'MQIsdp',
+    protocolVersion: 3
+};
+
+
+// create a socket object that listens on port 5000
+var io = require('socket.io').listen(3000);
+ console.log('Listen 3000');
+// create an mqtt client object and connect to the mqtt broker
+var client = mqtt.connect(options);
+  console.log('mqtt: ok');
+io.sockets.on('connection', function (socket) {
+    // socket connection indicates what mqtt topic to subscribe to in data.topic
+    socket.on('subscribe', function (data) {
+        console.log('Subscribing to '+data.topic);
+        socket.join(data.topic);
+        client.subscribe(data.topic);
+    });
+    // when socket connection publishes a message, forward that message
+    // the the mqtt broker
+    socket.on('publish', function (data) {
+        console.log('Publishing to '+data.topic);
+        client.publish(data.topic,data.payload);
+    });
+});
+ 
+// listen to messages coming from the mqtt broker
+client.on('message', function (topic, payload, packet) {
+    console.log(topic+'='+payload);
+    io.sockets.emit('mqtt',{'topic':String(topic),
+                            'payload':String(payload)});
+});
