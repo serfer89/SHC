cf247e1845df333bfdf1e0b824451a3ed8b26dc8
serfer89 <sergpavlov89@gmail.com>
2017-01-11 10:14:50 +0200

    answer try

--- server.js
+++ server.js
@@ -243,9 +243,10 @@ client.on('message', function(topic, payload, packet) {
     if (payload[0] == 's') {
         var op = 'update';
         var msg = payload[6] + "/" + payload[2];
+console.log("topic -"+payload[4]+" msg -"+msg)
         // topic - "device_id"; payload =state/room_id
-        var op = new query.mdb(op, payload[4], msg);
-        op.view();
+     /*   var op = new query.mdb(op, payload[4], msg);
+        op.view();*/
         console.log("Succsessful");
     } else {
         io.sockets.emit('mqtt', {
